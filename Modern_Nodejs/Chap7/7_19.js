a808052a1bc0f1f7564908bc8bb8652d6d9a48f3
kimth007kim <kimth007kim@naver.com>
2020-09-01 21:14:18 +0900

    Chap7 외부모듈

---
+++ Modern_Nodejs/Chap7/7_19.js
@@ -0,0 +1,25 @@
+// 모듈을 추출합니다.
+var http = require("http");
+var jade = require("jade");
+var fs = require("fs");
+
+// 서버를 생성하고 실행합니다.
+http
+  .createServer(function (request, response) {
+    // JadePage.jade 파일을 읽습니다.
+    fs.readFile("7_20.jade", "utf8", function (error, data) {
+      // jade 모듈을 사용합니다.
+      var fn = jade.compile(data);
+      // 출력합니다.
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(
+        fn({
+          name: "RintIanTta",
+          description: "Hello jade With Node.js .. !",
+        })
+      );
+    });
+  })
+  .listen(52273, function () {
+    console.log("Server Running at http://127.0.0.1:52273");
+  });
---
+++ Modern_Nodejs/Chap7/7_9.js
@@ -0,0 +1,3 @@
+// Node.js에서 가장 많이 사용되는 웹 프레임 워크인 express 프레임 워크가 템플릿엔진으로 ejs모듈과 jade를 자주사용함.
+// 모듈을 추출합니다.
+var jade = require("jade");
