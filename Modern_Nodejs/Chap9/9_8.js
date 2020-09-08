1ab3b492d9d839a62306eda465511f941eff91ff
kimth007kim <kimth007kim@naver.com>
2020-09-08 17:55:56 +0900

    Chap9 MySQL 데이터베이스 9-8까지

---
+++ Modern_Nodejs/Chap9/9_8.js
@@ -0,0 +1,33 @@
+// 모듈을 추출합니다.
+var fs = require("fs");
+var ejs = require("ejs");
+var mysql = require("mysql");
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// 데이터베이스와 연결합니다.
+var client = mysql.createConnection({
+  user: "root",
+  password: "1234",
+  database: "Company",
+});
+
+// 서버를 생성합니다.
+var app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+// 서버를 실행합니다.
+app.listen(52273, function () {
+  console.log("Server running at http://127.0.0.1:52273");
+});
+
+app.get("/", function (request, response) {});
+app.get("/delete/:id", function (request, response) {});
+app.get("/insert", function (request, response) {});
+app.post("/insert", function (request, response) {});
+app.get("/edit/:id", function (request, response) {});
+app.post("/edit/:ids", function (request, response) {});
