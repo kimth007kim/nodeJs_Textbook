df8f34485a9bca3fa35c692ff6489ae73611426d
kimth007kim <kimth007kim@naver.com>
2020-11-15 22:47:46 +0900

    노드 js 생활코딩 2번쨰 쿠키

---
+++ Nodejs-master/Nodejs-master/nodejs/cookie.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var cookie = require("cookie");
+http
+  .createServer(function (request, response) {
+    var cookies = {};
+    if (request.headers.cookie !== undefined) {
+      console.log(request.headers.cookie);
+      cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
+    response.writeHead(200, {
+      "Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"],
+    });
+    response.end("Cookie!!");
+  })
+  .listen(3000);
